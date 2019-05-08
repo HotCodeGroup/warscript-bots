d1c0d46564b3bfa25e260429e69166f62f542c89
GDVFox <gaaveer@gmail.com>
2019-05-08 12:03:20 +0300

    [ADD] Update structure

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/HotCodeGroup/warscript-bots
 go 1.12
 
 require (
-	github.com/HotCodeGroup/warscript-utils v0.0.0-20190507114736-1bff09fdeb6e
+	github.com/HotCodeGroup/warscript-utils v0.0.0-20190508085920-18ec01636892
 	github.com/go-park-mail-ru/2019_1_HotCode v0.0.0-20190426172604-1d3ce9818cea
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/mux v1.7.1
--- sql/bots.sql
+++ sql/bots.sql
@@ -15,8 +15,10 @@ CREATE TABLE "bots"
     is_verified BOOLEAN NOT NULL DEFAULT FALSE,
 	author_id BIGINT NOT NULL,
 	game_slug citext CONSTRAINT game_slug_empty NOT NULL CHECK ( game_slug <> '' ),
+	score INTEGER NOT NULL DEFAULT 0,
+	games_played INTEGER NOT NULL DEFAULT 0,
 
 	CONSTRAINT unique_code UNIQUE (code, language, author_id, game_slug)
 );
 
-ALTER TABLE bots OWNER TO warscript_games_user;
+ALTER TABLE bots OWNER TO warscript_bots_user;
