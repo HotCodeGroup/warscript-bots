7c9094a385a913c91b11cc157a50e3f9651fd12c
GDVFox <gaaveer@gmail.com>
2019-05-30 10:56:49 +0300

    [ADD] Start logs

--- main.go
+++ main.go
@@ -124,8 +124,13 @@ func main() {
 		Port:    httpPort,
 		Address: "127.0.0.1",
 	})
+	if err != nil {
+		logger.Errorf("can not register warscript-bots-http: %s", err.Error())
+		return
+	}
 	defer deregisterService(consul, httpServiceID)
 
+	logger.Info("connecting grpc auth...")
 	authGPRCConn, err := balancer.ConnectClient(consul, "warscript-users-grpc")
 	if err != nil {
 		logger.Errorf("can not connect to auth grpc: %s", err.Error())
@@ -134,6 +139,7 @@ func main() {
 	defer authGPRCConn.Close()
 	authGPRC = models.NewAuthClient(authGPRCConn)
 
+	logger.Info("connecting grpc games...")
 	gamesGPRCConn, err := balancer.ConnectClient(consul, "warscript-games-grpc")
 	if err != nil {
 		logger.Errorf("can not connect to games grpc: %s", err.Error())
@@ -142,6 +148,7 @@ func main() {
 	defer gamesGPRCConn.Close()
 	gamesGPRC = models.NewGamesClient(gamesGPRCConn)
 
+	logger.Info("connecting grpc notify...")
 	notifyGRPCConn, err := balancer.ConnectClient(consul, "warscript-notify-grpc")
 	if err != nil {
 		logger.Errorf("can not connect to notify grpc: %s", err.Error())
